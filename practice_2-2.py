3f6168c83d93b255e021c8a55f141d6ba862311a
cuberisu <cuberisu@gmail.com>
2021-10-26 23:57:02 +0900

    2장 1~2

---
+++ practice_2-2.py
@@ -0,0 +1,6 @@
+# 2-2 입력받은 숫자들의 평균
+
+num1 = int(input("첫 번째 숫자를 입력하세요: "))
+num2 = int(input("두 번째 숫자를 입력하세요: "))
+num3 = int(input("세 번째 숫자를 입력하세요: "))
+print(num1, num2, num3, "의 평균은", (num1+num2+num3)/2, "입니다.")
