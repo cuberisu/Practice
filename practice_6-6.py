895fe56303bb059ae9d716bd64c3f50c74fc8b7e
cuberisu <cuberisu@gmail.com>
2021-11-17 23:59:43 +0900

    6. loop (5~6)

---
+++ practice_6-6.py
@@ -0,0 +1,7 @@
+# 주사위 2개 던지기
+import random
+sq1 = random.randint(1, 6) # 1이랑 6이 포함되는 정수범위의 난수
+sq2 = random.randint(1, 6) # 1이랑 6이 포함되는 정수범위의 난수
+print("주사위 2개 던지기!")
+print("주사위 1:", sq1)
+print("주사위 2:", sq2)
